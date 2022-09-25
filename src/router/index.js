46a078192a5d652fac7b7b4c6c7c1cc55448cca0
Gino-Y <ginoyan250@gmail.com>
2022-09-25 23:39:59 +0800

    220925

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
         path: '/home',
         name:'home',
         components:{
-            default:()=> Home,
+            default:()=> import('../components/views/home.vue'),
             axios:()=> import('../components/vueadv-axios.vue'),
             computed:()=> import('../components/vueadv-computed.vue'),
         }
@@ -46,6 +46,17 @@ const router = createRouter({
     history: createWebHashHistory(), // 路由的模式 哈希模式#
     linkActiveClass:'active', // 路由样式
     routes // 路由配置
+})
+
+// 使用router.beforeEach 注册一个全局前置首位
+router.beforeEach((to, from, next)=>{
+    console.log(to) //to.path
+    next() // 下一步操作
+    // if(to.path !='/home'){
+    //     next({path:'/home'})
+    // }else {
+    //     next()
+    // }
 })
 
 // 导出

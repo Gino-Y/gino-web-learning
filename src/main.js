f958e8ac06ffc17d3c62ccb1f58caa34833f8080
Gino-Y <ginoyan@gmail.com>
2022-10-16 20:24:39 +0800

    221014

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue' // 导入入口组件
 import router from "./router/index"; // 导入路由
 import {createPinia} from "pinia";
 import piniaPluginPersist from 'pinia-plugin-persist' // keep data
-// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersist) // use keep data
-// pinia.use(piniaPluginPersistedstate) // use keep data
-createApp(App).use(router).use(pinia).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.use(pinia)
+app.mount('#app')
+// createApp(App).use(router).use(pinia).mount('#app')
